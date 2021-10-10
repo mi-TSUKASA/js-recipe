e8e49f482229ade967f99a7b3f392f0182739912
mi-TSUKASA <mi.tsukasa0898@gmail.com>
2021-10-10 18:57:00 +0900

    ゼミで自販機を作りました

--- 02_レシピ/02_DogAPIで非同期処理/main.js
+++ 02_レシピ/02_DogAPIで非同期処理/main.js
@@ -13,3 +13,17 @@ fetch("https://dog.ceo/api/breeds/image/random")
   const dogs = await res.json()
   imageElement.src = dogs.message
 }*/
+const aisatsu = ["こんにちは", "おはよう"]
+const miyake = ["三宅", "司"]
+aisatsu.push("こんばんは")
+for (let i = 0; i < aisatsu.length; i++) {
+  console.log(aisatsu[i])
+}
+aisatsu.pop()
+console.log(aisatsu)
+aisatsu.splice(0, 1, "あほ")
+console.log(aisatsu)
+aisatsu.splice(2, 0, "Hello")
+console.log(aisatsu)
+const miyakeAisatsu = [...aisatsu, ...miyake]
+console.log(miyakeAisatsu)
--- 02_レシピ/02_Trello（保存機能なし）/main.js
+++ 02_レシピ/02_Trello（保存機能なし）/main.js
@@ -2,10 +2,23 @@ const inputElement = document.getElementById("input-todo")
 const container = document.getElementById("cards-container")
 const addButton = document.getElementById("add-button")
 
-addButton.onclick = function() {
-  const card = createCard(inputElement.value)
-  container.append(card)
-  inputElement.value = ""
+const createCard = function(text) {
+  const card = document.createElement("div")
+  card.className = "card"
+  const todo = document.createElement("div")
+  todo.className = "todo"
+  todo.textContent = text
+  card.append(todo)
+
+  const deleteButton = document.createElement("div")
+  deleteButton.className = "delete"
+
+  deleteButton.onclick = function() {
+    card.remove()
+  }
+  card.append(deleteButton)
+
+  return card
 }
 
 inputElement.onkeydown = function(event) {
@@ -16,21 +29,10 @@ inputElement.onkeydown = function(event) {
   }
 }
 
-const createCard = function(text) {
-  const card = document.createElement("div")
-  card.className = "card"
-
-  const todo = document.createElement("div")
-  todo.className = "todo"
-  todo.textContent = text
-  card.append(todo)
-
-  const deleteButton = document.createElement("div")
-  deleteButton.className = "delete"
-  deleteButton.onclick = function() {
-    card.remove()
-  }
-  card.append(deleteButton)
-
-  return card
+addButton.onclick = function() {
+  const card = createCard(inputElement.value)
+  container.append(card)
+  inputElement.value = ""
 }
+
+//2つ目のカード追加
